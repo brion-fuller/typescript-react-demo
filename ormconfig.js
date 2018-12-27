ce240dcf0eaf9ba6c49c9d289dc80be162549734
Brion Fuller <fuller@Brions-MacBook-Pro.local>
2018-12-26 20:45:32 -0800

    initial commit

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+    cli: {
+      migrationsDir: `server/database/migrations`,
+    },
+    database: "postgres",
+    entities: [`${__dirname}/server/database/entities/*.ts`],
+    host: "localhost",
+    logging: false,
+    migrations: [`${__dirname}/server/database/migrations/*.ts`],
+    name: "default",
+    password: "admin",
+    port: 5432,
+    type: "postgres",
+    username: "postgres",
+  };
